ce5427d51805ced79973de9989048316a9cc2503
lixia <1270634123@qq.com>
2017-02-28 13:53:17 +0800

    提交

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var path = require('path');//引用模板引擎使用
+
 var express = require('express');
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));// 设置存放模板文件的目录
+app.set('view engine', 'ejs');// 设置模板引擎为 ejs
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:name',function(req,res){
-	res.send('user name is：'+ req.params.name);
+router.get('/:name',function(req,res){   //res.render 渲染ejs模板，第一个参数对应模板名称
+	res.render('user', {
+	    name: req.params.name
+	  });
 });
 
 module.exports = router;
